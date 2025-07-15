7bf4a85ae95e0d4f93f1f287d9b73c1db82838a6
Krystian Pietron <krystian.pietron@elcaro.pl>
2025-07-15 17:54:11 +0200

    changing addon config and fix structure

---
+++ addon-i2c/rootfs/etc/services.d/i2c/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/with-contenv bashio
+
+echo "Starting I2C add-on..."
+
+python3 /app/main.py
+
+echo "I2C add-on finished."
--- addon-i2c/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-echo "Starting add-on i2c..."
-
-python3 /main.py
-
-echo "Python add-on finished."
